e0474d7db179769d2810a5bb457795ec77115737
Alfusainey <alf.jallow@gmail.com>
2015-03-23 02:40:15 +0100

    Update HttpConstants.java

--- src/main/java/com/adobe/assignment/http/HttpConstants.java
+++ src/main/java/com/adobe/assignment/http/HttpConstants.java
@@ -10,15 +10,9 @@ public interface HttpConstants {
     public static final String HTTP_VERSION = "HTTP/1.1";
     
     //--------------------------------< Methods constants >---
-    public static final int METHOD_GET_CODE = 1;
     public static final String METHOD_GET = "GET";
-    
-    public static final int METHOD_PUT_CODE = METHOD_GET_CODE + 1;
     public static final String METHOD_PUT = "PUT";
-    
-    public static final int METHOD_DELETE_CODE = METHOD_PUT_CODE + 1;
     public static final String METHOD_DELETE = "DELETE";
-    
     public static final String METHOD_OPTIONS = "OPTIONS";
     
     
